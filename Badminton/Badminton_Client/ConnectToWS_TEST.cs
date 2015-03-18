5916b7c6e95627ea60b6defb1cf7512b770a19ed
Rasmus Ketelsen <rkketelsen@gmail.com>
2015-03-18 20:54:20 +0100

    Added test class

---
+++ Badminton/Badminton_Client/ConnectToWS_TEST.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Badminton_Client
+{
+    class ConnectToWS_TEST
+    {
+        ////Load service
+        //    Eksperiment_3_WS service = new Eksperiment_3_WS();
+
+        //    //Get worker id, dictionary and some work to do
+        //    _mySlaveId = service.GetUniqueWorkerId();
+        //    Console.WriteLine("Slave id: " + _mySlaveId);
+
+        //    _dictionary = service.GetDictionaryPart(_mySlaveId);
+        //    Console.WriteLine("Got dictionary");
+
+        //    _currentWork = service.GetWork();
+    }
+}
