2bbcb959a41d37760b28c342fe7ef566a5bb5116
Rasmus Ketelsen <rkketelsen@gmail.com>
2015-03-23 15:32:58 +0100

    Updated member properties Changed everything to english

---
+++ Badminton/Badminton_Client/Control/AddMemberHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Badminton_Client.Control
+{
+    public class AddMemberHandler
+    {
+        private MemberCatalog _memberCatalog;
+
+        public AddMemberHandler()
+        {
+            _memberCatalog = new MemberCatalog();
+        }
+
+        public void AddMember(string firstName, string surName, int cpr, string address, int zipCode, string phone, string mail)
+        {
+            _memberCatalog.AddMember(new Member(firstName, surName, cpr, address, zipCode, phone, mail));
+        }
+    }
+}
---
+++ Badminton/Badminton_Client/Model/Member.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Badminton_Client
+{
+    public class Member
+    {
+        private string _firstName;
+        private string _surName;
+        private int _cpr;
+        private string _address;
+        private int _zipCode;
+        private string _phone;
+        private string _mail;
+
+        public Member(string firstName, string surName, int cpr, string address, int zipCode, string phone, string mail)
+        {
+            _firstName = firstName;
+            _surName = surName;
+            _cpr = cpr;
+            _address = address;
+            _zipCode = zipCode;
+            _phone = phone;
+            _mail = mail;
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string SurName
+        {
+            get { return _surName; }
+            set { _surName = value; }
+        }
+
+        public int Cpr
+        {
+            get { return _cpr; }
+            set { _cpr = value; }
+        }
+
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+
+        public int ZipCode
+        {
+            get { return _zipCode; }
+            set { _zipCode = value; }
+        }
+
+        public string Phone
+        {
+            get { return _phone; }
+            set { _phone = value; }
+        }
+
+        public string Mail
+        {
+            get { return _mail; }
+            set { _mail = value; }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Navn: {0} {1}", _firstName, _surName);
+        }
+    }
+}
